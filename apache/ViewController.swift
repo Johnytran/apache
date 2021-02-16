c52268316da7108f878b3f8955d07baf1a8a3eca
andy <tuananhasp@gmail.com>
2021-02-16 18:48:58 +1100

    update core ml to model

--- apache/ViewController.swift
+++ apache/ViewController.swift
@@ -5,15 +5,19 @@
 //  Created by Owner on 10/2/21.
 //
 
-import UIKit
 import SceneKit
 import ARKit
+import Vision
 
 class ViewController: UIViewController, ARSCNViewDelegate, ARSessionDelegate {
     
     
     @IBOutlet weak var arView: ARSCNView!
     var sceneController = MainScene()
+    let currentMLModel = FistClassifier364().model;
+    private let serialQueue = DispatchQueue(label: "com.aboveground.dispatchqueueml")
+    private var visionRequests = [VNRequest]()
+    private var timer = Timer()
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -40,7 +44,7 @@ class ViewController: UIViewController, ARSCNViewDelegate, ARSessionDelegate {
     @objc func didTapScreen(recognizer: UITapGestureRecognizer) {
         if let camera = arView.session.currentFrame?.camera {
             var translation = matrix_identity_float4x4
-            translation.columns.3.z = -5.0
+            translation.columns.3.z = -7.0
             let transform = camera.transform * translation
             let position = SCNVector3(transform.columns.3.x, transform.columns.3.y, transform.columns.3.z)
             sceneController.addApache(parent: arView.scene.rootNode, position: position)
@@ -52,5 +56,51 @@ class ViewController: UIViewController, ARSCNViewDelegate, ARSessionDelegate {
 
         // Run the view's session
         arView.session.run(configuration);
+        
+        
+        setupCoreML();
+
+        timer = Timer.scheduledTimer(timeInterval: 0.1, target: self, selector: #selector(self.loopCoreMLUpdate), userInfo: nil, repeats: true)
+    }
+    
+    private func setupCoreML() {
+        
+        guard let selectedModel = try? VNCoreMLModel(for: currentMLModel) else {
+            fatalError("Could not load model.")
+        }
+        
+        let classificationRequest = VNCoreMLRequest(model: selectedModel,
+                                                    completionHandler: classificationCompleteHandler)
+        classificationRequest.imageCropAndScaleOption = VNImageCropAndScaleOption.centerCrop // Crop from centre of images and scale to appropriate size.
+        visionRequests = [classificationRequest]
+    }
+    private func updateCoreML() {
+        let pixbuff : CVPixelBuffer? = (arView.session.currentFrame?.capturedImage)
+        if pixbuff == nil { return }
+        
+        let deviceOrientation = UIDevice.current.orientation.getImagePropertyOrientation()
+        let imageRequestHandler = VNImageRequestHandler(cvPixelBuffer: pixbuff!, orientation: deviceOrientation,options: [:])
+        do {
+            try imageRequestHandler.perform(self.visionRequests)
+        } catch {
+            print(error)
+        }
+        
+    }
+    @objc private func loopCoreMLUpdate() {
+       serialQueue.async {
+           self.updateCoreML()
+       }
+   }
+}
+extension UIDeviceOrientation {
+    func getImagePropertyOrientation() -> CGImagePropertyOrientation {
+        switch self {
+        case UIDeviceOrientation.portrait, .faceUp: return CGImagePropertyOrientation.right
+        case UIDeviceOrientation.portraitUpsideDown, .faceDown: return CGImagePropertyOrientation.left
+        case UIDeviceOrientation.landscapeLeft: return CGImagePropertyOrientation.up
+        case UIDeviceOrientation.landscapeRight: return CGImagePropertyOrientation.down
+        case UIDeviceOrientation.unknown: return CGImagePropertyOrientation.right
+        }
     }
 }
